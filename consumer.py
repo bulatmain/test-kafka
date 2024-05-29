9418a7761f3602b3f6841bfdb148a347b58e1791
bulat <sinntexxx@gmail.com>
2024-05-29 21:08:18 +0300

    123

--- consumer.py
+++ consumer.py
@@ -2,23 +2,27 @@ from kafka import KafkaConsumer
 import json
 import os
 
+print('consumer!')
+
 # Configuration for Kafka Consumer
-kafka_bootstrap_server = os.environ.get('KAFKA_BOOTSTRAP_SERVER')
+# kafka_bootstrap_server = os.environ.get('KAFKA_BOOTSTRAP_SERVER')
+kafka_bootstrap_server = 'broker:9092'
 topic_name = 'test-topic'        # Replace with your Kafka topic
 
 # Create an instance of the Kafka consumer
 consumer = KafkaConsumer(
-    topic_name,
-    bootstrap_servers=kafka_bootstrap_server,
-    auto_offset_reset='earliest',  # Start from the earliest messages
-    enable_auto_commit=True,
-    group_id='my-group',           # Consumer group id
+    bootstrap_servers=[kafka_bootstrap_server],
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
+
+consumer.subscribe(topic_name)
+
+print('Consumer launched!')
+
+while True:
+    data = next(consumer)
+    print(data, data.value)
 
 # Process messages
 for message in consumer:
     print(f"Received message from topic '{message.topic}': {message.value}")
-
-# Clean up: the consumer will continue listening until the program is interrupted or killed.
-# In a real-world application, ensure proper shutdown and exception handling.
--- producer.py
+++ producer.py
@@ -2,10 +2,13 @@ from kafka import KafkaProducer
 import json
 import time
 import os
+from time import sleep
+
+print('producer!')
 
 # Configuration for Kafka Producer
-# Получение адреса Kafka из переменной окружения
-kafka_bootstrap_server = os.environ.get('KAFKA_BOOTSTRAP_SERVER')
+# kafka_bootstrap_server = os.environ.get('KAFKA_BOOTSTRAP_SERVER')
+kafka_bootstrap_server = 'broker:9092'
 topic_name = 'test-topic'        # Replace with your Kafka topic
 
 # Create an instance of the Kafka producer
@@ -14,12 +17,18 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# Send a message
-message = {"text": "Hello, Consumer!"}
-producer.send(topic_name, message)
-producer.flush()  # Ensure all messages are sent
+print("Producer launched!")
 
-print(f"Sent message to topic '{topic_name}': {message}")
+i = 0
+while True:
+    # Send a message
+    print(f"Sending message {i}...")
+    message = {f"Message {i}": "Hello, Consumer!"}
+    producer.send(topic_name, message)
+    producer.flush()  # Ensure all messages are sent
+    print(f"Sent message to topic '{topic_name}': {message}")
+    sleep(1)
+    i += 1
 
 # Close the producer connection
 producer.close()
